5da2bbe72be61c78853fef616131593c1305e5d1
tejasmal56 <tejasmal56@gmail.com>
2022-08-01 18:28:53 +0000

    commit seq_detector changes

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import cocotb
 from cocotb.clock import Clock
+from cocotb.triggers import Timer
 from cocotb.triggers import RisingEdge, FallingEdge
 
 @cocotb.test()
@@ -41,30 +42,64 @@ async def test_seq_bug1(dut):
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
 
+    
+
+    await RisingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    # await RisingEdge(dut.clk)
+    queue.append(dut.inp_bit.value)
+    print("queue inside if block:",queue)
+    await RisingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    queue.append(dut.inp_bit.value)
+    await RisingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    queue.append(dut.inp_bit.value)
     await RisingEdge(dut.clk)
     dut.inp_bit.value = 1
     queue.append(dut.inp_bit.value)
     await RisingEdge(dut.clk)
     dut.inp_bit.value = 0
-    await RisingEdge(dut.clk)
-    dut.inp_bit.value = 1
-    await RisingEdge(dut.clk)
-    dut.inp_bit.value = 1
+    queue.append(dut.inp_bit.value)
     await RisingEdge(dut.clk)
     dut.inp_bit.value = 0
-    await RisingEdge(dut.clk)
-    dut.inp_bit.value = 0
-    await RisingEdge(dut.clk)
-    dut.inp_bit.value = 1
+    queue.append(dut.inp_bit.value)
     await RisingEdge(dut.clk)
     dut.inp_bit.value = 1
+    queue.append(dut.inp_bit.value)
+    await RisingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    queue.append(dut.inp_bit.value)
+    print("queue inside if block:",queue)
 
-    def seqChecker():
-            if():
-                result =  inp[i]
-                print("result inside if block:",result)
-        
+    # def seqChecker(queue):
+    #     result = []
+    #     if(queue==1011):
+    #         result[0] =  queue.pop(0)
+    #         result[1] =  queue.pop(0)
+    #         result[2] =  queue.pop(0)
+    #         result[3] =  queue.pop(0)
+    #         print("result inside if block:",result)
+    #     return result
+    def seqChecker(queue):
+        # result = []
+        if(queue==1011):
+            # result[0] =  queue.pop(0)
+            # result[1] =  queue.pop(0)
+            # result[2] =  queue.pop(0)
+            # result[3] =  queue.pop(0)
+            result = 1
+   
+            print("result inside if block:",result)
+        return result    
 
-        await Timer(2, units='ns')
-        assert dut.seq_seen.value == seqChecker(sel,i), "Randomised test failed with: {sel} != {out}".format(
-            sel=dut.sel.value, out=dut.out.value)    
+    # await Timer(2, units='ns')
+    # await RisingEdge(dut.clk)
+    # assert dut.seq_seen.value == seqChecker(queue), "Randomised test failed with: {seq_seen} != {1011}".format(
+    #     seq_seen=dut.seq_seen.value, inp_bit=dut.inp_bit.value)  
+
+
+    
+    await RisingEdge(dut.clk)
+    assert dut.seq_seen.value == seqChecker(queue), "Randomised test failed with: {seq_seen} != {1011}".format(
+        seq_seen=dut.seq_seen.value, inp_bit=dut.inp_bit.value)    
